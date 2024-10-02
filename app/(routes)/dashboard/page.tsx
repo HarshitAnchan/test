fdd69804ba72c7ed5d10b93be0f2d9d6d0ab29ad
harshitanchan <harshitanchan647@gmail.com>
2024-10-03 00:45:26 +0530

    build issue

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -9,7 +9,6 @@ import { useConvex, useMutation, useQuery } from "convex/react";
 import React, { useEffect } from "react";
 import Header from "./_components/Header";
 import FileList from "./_components/FileList";
-import AdBanner from "./../../_components/AdBanner";
 function Dashboard() {
   const convex = useConvex();
   const { user }: any = useKindeBrowserClient();
